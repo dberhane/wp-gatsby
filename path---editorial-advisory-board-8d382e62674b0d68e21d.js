b7501da60eccf55907bb7d396ffcbad843c6af00
dberhane <dberhane@bmj.com>
2018-09-11 11:22:30 +0100

    Updates

---
+++ path---editorial-advisory-board-8d382e62674b0d68e21d.js
@@ -0,0 +1,2 @@
+webpackJsonp([0xd21325ecab42],{624:function(t,e){t.exports={data:{wordpressPage:{title:"Editorial advisory board",content:'<p>&nbsp;</p>\n<p><a href="http://bmjopen.bmj.com/site/about/edboard.xhtml" target="_blank">Click here for the BMJ Open editorial board</a>.<!--TrendMD v2.4.6--></p>\n',date:"September 24, 2010"},site:{id:"Site",siteMetadata:{title:"Gatsby Default Starter"}}},pathContext:{id:"3371c4b8-ca65-56f2-8d6d-ee43464c5c31"}}}});
+//# sourceMappingURL=path---editorial-advisory-board-8d382e62674b0d68e21d.js.map
---
+++ path---instructions-for-authors-9b279639110ca223c97d.js
@@ -0,0 +1,2 @@
+webpackJsonp([46061863143596],{664:function(t,e){t.exports={data:{wordpressPage:{title:"Instructions for authors",content:'<p><strong> </strong></p>\n<p><a href="http://bmjopen.bmj.com/site/about/guidelines.xhtml" target="_blank">Click here for BMJ Open&#8217;s Instructions for authors.</a></p>\n<p>&nbsp;<!--TrendMD v2.4.6--></p>\n',date:"August 31, 2010"},site:{id:"Site",siteMetadata:{title:"Gatsby Default Starter"}}},pathContext:{id:"63413bc4-6b1e-586c-8afc-833e80f7f3dd"}}}});
+//# sourceMappingURL=path---instructions-for-authors-9b279639110ca223c97d.js.map
